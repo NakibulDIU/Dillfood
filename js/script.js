f1b65fedb7bc0f95d140ffd5a6ed15669d89434c
NakibulDIU <nakibulislam@students.diu.ac>
2025-08-15 21:16:27 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// Make mobile navigation work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function(){
+    headerEl.classList.toggle("nav-open");
+});
+
+// //////////////////////
+// Smooth scrolling animation
+
+const alllinks = document.querySelectorAll("a:link");
+
+alllinks.forEach(function(link){
+    link.addEventListener("click", function(e){
+        e.preventDefault();
+        const href = link.getAttribute("href");
+        console.log(href);
+
+        // Scroll back to top
+        if (href == "#")
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+            });
+
+            
+        // Scroll to other links    
+        
+        if (href!== "#" && href.startsWith("#")) {
+            const sectionEl = document.querySelector(href);
+            sectionEl.scrollIntoView({behavior:"smooth"});
+        }
+
+        //close mobile navigation
+
+        if (link.classList.contains("main-nav-link"))
+            headerEl.classList.toggle("nav-open");
+            
+    });
+});
+
